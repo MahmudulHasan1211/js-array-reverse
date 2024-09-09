1ed477438f1ab87380cf5b0d1cbfe403b1f1d8fb
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-09 22:58:00 +0600

    first commit

---
+++ array reverse.js
@@ -0,0 +1,15 @@
+// array reverse 
+ let num = [11, 22, 33, 44, 55, 66, 77, 88, 99]
+
+
+//  reverse using reverse method 
+//  console.log(num);
+ num.reverse()
+ console.log(num)
+
+// //  reverse using for of 
+
+//  let rev_num = [];
+//   for ( let num of num ){
+//     console.log(num);
+//   }
